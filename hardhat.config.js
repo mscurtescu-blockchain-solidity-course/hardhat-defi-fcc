b7c97500b9c0e93b8e0e67809041159ffd21d216
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-12-19 13:14:12 -0800

    add hardhat config from hardhat-smartcontract-lottery-fcc

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,62 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
+
+/** @type string */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // }
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
   solidity: "0.8.19",
-};
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH", // "MATIC",
+  },
+  mocha: {
+    timeout: 500000, // 500 seconds max for running tests
+  },};
