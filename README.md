761594bcce8bee67e77929d9385fd35fdf4718f6
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-12-19 22:15:11 -0800

    cleanup and more notes

--- README.md
+++ README.md
@@ -21,6 +21,11 @@ https://github.com/PatrickAlphaC/hardhat-defi-fcc
  * but ETH is not an ERC20, hence WETH
  * https://weth.io/
  * WETH contract on Sepolia: https://sepolia.etherscan.io/token/0xd0df82de051244f04bff3a8bb1f62e1cd39eed92
-   * deposit ETH to contract to get WETH back: https://sepolia.etherscan.io/address/0xd0df82de051244f04bff3a8bb1f62e1cd39eed92#writeContract 
+   * deposit ETH to contract to get WETH back: https://sepolia.etherscan.io/address/0xd0df82de051244f04bff3a8bb1f62e1cd39eed92#writeContract
+* AAVE v2 is used
+  * the v3 API is different, but v3 is deployed to Sepolia
+  * v2 is not deployed to Sepolia, only to Goerli
+* Mainnet contract and token addresses are used
+  * the hardhat network is a Mainnet fork
 * stable borrowing and repaying not enabled (anymore I guess)
   * the `borrow` and `repay` calls must specify `2` (for variable) instead of `1` (for stable)
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,7 @@ require("hardhat-deploy")
 require("dotenv").config()
 
 /** @type string */
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-/** @type string */
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
-/** @type string */
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-/** @type string */
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -21,19 +14,6 @@ module.exports = {
         url: MAINNET_RPC_URL
       },
       blockConfirmations: 1,
-    },
-    localhost: {
-      chainId: 31337,
-    },
-    sepolia: {
-      chainId: 11155111,
-      url: SEPOLIA_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      blockConfirmations: 6,
     },
   },
   solidity: {
@@ -53,24 +33,5 @@ module.exports = {
     deployer: {
       default: 0,
     },
-    player: {
-      default: 1,
-    },
   },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-    },
-  },
-  gasReporter: {
-    enabled: false,
-    outputFile: "gas-report.txt",
-    noColors: true,
-    currency: "USD",
-    // coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "ETH", // "MATIC",
-  },
-  mocha: {
-    timeout: 500000, // 500 seconds max for running tests
-  },};
+};
